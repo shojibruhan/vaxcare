74b56f67556e10fab623cad15adb6ba83bc9c6f3
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-03-29 22:58:07 +0600

    setup djsoer

--- vax_care/settings.py
+++ vax_care/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "djoser",
     "api",
     "users",
     "campaigns",
@@ -117,3 +118,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
+    ),
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+DJOSER = {
+    'SERIALIZERS': {},
+}
